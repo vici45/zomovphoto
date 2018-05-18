495d762429d47016f265acd266d558beb2d0d0ea
vici45 <vici_45@sina.com>
2018-05-18 12:50:21 +0800

    first commit project init

---
+++ src/main/java/com/vici/zomovphoto/ZomovphotoApplication.java
@@ -0,0 +1,12 @@
+package com.vici.zomovphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomovphotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomovphotoApplication.class, args);
+	}
+}
